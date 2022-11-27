24e19c156ea9841950eceeb49e7d68bb520ec1cf
gastonalt <gxsworld@gmail.com>
2022-11-27 12:52:50 -0300

    Ahora el state persiste en el storage

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store'
 import { Provider } from 'react-redux'
+import {PersistGate} from "redux-persist/integration/react";
+import {persistStore} from "redux-persist";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let persistor = persistStore(store)
+
 root.render(
   <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
   </React.StrictMode>
 );
--- src/store.js
+++ src/store.js
@@ -1,11 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
 import todoListSlice from './states/todoListSlice'
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+const persistConfig = {
+    key: 'todos',
+    version: 1,
+    storage
+}
+
+const reducer = combineReducers({
+    todoList: todoListSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export default configureStore({
-    reducer: {
-        todoList: todoListSlice,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
-      }),
+    }),
 })
